5bab3980f1e7ba0e1b4182772aa4a97274946236
satel31 <blokhnina.tatiana@yandex.ru>
2023-08-05 23:04:16 +0300

    Created models for habit and user

--- apps/habits/models.py
+++ apps/habits/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from apps.users.models import User
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Habit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='User')
+    place = models.CharField(max_length=255, verbose_name='Place')
+    time = models.DateTimeField(verbose_name='Time')
+    action = models.CharField(max_length=255, verbose_name='Action')
+    is_pleasant = models.BooleanField(default=False, verbose_name='Is pleasant')
+    related_habit = models.ForeignKey('Habit', on_delete=models.SET_NULL, **NULLABLE, verbose_name='Related habit')
+    frequency = models.IntegerField(default=1, verbose_name='Frequency')
+    reward = models.CharField(max_length=255, verbose_name='Reward')
+    execution_time = models.IntegerField(**NULLABLE, verbose_name='Execution time')
+    is_public = models.BooleanField(default=False, verbose_name='Is public')
+
+    def __str__(self):
+        return f'I will {self.action} at {self.time} in {self.place}'
+
+    class Meta:
+        verbose_name = 'habit'
+        verbose_name_plural = 'habits'
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    telegram = models.CharField(max_length=150, verbose_name='Telegram')
+    REQUIRED_FIELDS = ["email", "telegram"]
