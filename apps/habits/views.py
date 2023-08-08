8cf984b8181fc44895aa6214b712f48614eda18f
satel31 <blokhnina.tatiana@yandex.ru>
2023-08-08 23:37:16 +0300

    Added endpoints

--- apps/habits/views.py
+++ apps/habits/views.py
@@ -1,4 +1,3 @@
-from django.db.models import Q
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
@@ -24,24 +23,32 @@ class HabitListAPIView(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return Habit.objects.filter(Q(owner=self.request.user) | Q(is_public=True))
+        return Habit.objects.filter(user=self.request.user)
+
+class HabitPublicListAPIView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    pagination_class = HabitsPagination
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Habit.objects.filter(is_public=True)
 
 class HabitDetailAPIView(generics.RetrieveAPIView):
     serializer_class = HabitSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return Habit.objects.filter(Q(owner=self.request.user) | Q(is_public=True))
+        return Habit.objects.filter(user=self.request.user)
 
 class HabitUpdateAPIView(generics.UpdateAPIView):
     serializer_class = HabitSerializer
     permission_classes = [IsAuthenticated, IsOwnerPermission]
 
     def get_queryset(self):
-        return Habit.objects.filter(owner=self.request.user)
+        return Habit.objects.filter(user=self.request.user)
 
-class LessonDeleteAPIView(generics.DestroyAPIView):
+class HabitDeleteAPIView(generics.DestroyAPIView):
     permission_classes = [IsAuthenticated, IsOwnerPermission]
 
     def get_queryset(self):
-        return Habit.objects.filter(owner=self.request.user)
+        return Habit.objects.filter(user=self.request.user)
