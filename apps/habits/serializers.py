0708a9fcb98f94254e688408674cfeafe2ba771d
satel31 <blokhnina.tatiana@yandex.ru>
2023-08-08 23:59:38 +0300

    Fixed validators for update

--- apps/habits/serializers.py
+++ apps/habits/serializers.py
@@ -5,12 +5,35 @@ from apps.habits.validators import *
 
 
 class HabitSerializer(serializers.ModelSerializer):
+
+    def validate(self, data):
+        if self.instance.related_habit and (data.get('reward') or data.get('related_habit')):
+            raise serializers.ValidationError('You cannot choose a reward and related habit for one habit')
+
+
+        elif self.instance.is_pleasant and (data.get('reward') or data.get('related_habit')):
+
+            raise serializers.ValidationError('A pleasant habit cannot have a reward or an related habit')
+
+
+        elif data.get('is_pleasant') and (self.instance.reward or self.instance.related_habit):
+            raise serializers.ValidationError('A pleasant habit cannot have a reward or an related habit')
+
+
+        elif data.get('related_habit'):
+            if data.get('related_habit').is_pleasant is False:
+                raise serializers.ValidationError('A related habit should be pleasant')
+
+        elif data.get('execution_time'):
+            if data.get('execution_time') > 120:
+                raise serializers.ValidationError('Execution time of your habit should be no more than 120 seconds')
+
+        elif data.get('frequency'):
+            if data.get('frequency') > 7:
+                raise serializers.ValidationError('You cannot perform a habit less than 1 time in 7 days.')
+
+        return data
+
     class Meta:
         model = Habit
         fields = '__all__'
-        validators = [
-            RewardRelatedHabitValidator(),
-            ExecutionTimeValidator(field='execution_time'),
-            IsPleasantValidator(),
-            FrequencyValidator(field='frequency')
-        ]
