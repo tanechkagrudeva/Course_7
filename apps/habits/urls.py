52bcce98a3332271abac45eca74f1dfe8f4c305d
satel31 <blokhnina.tatiana@yandex.ru>
2023-08-08 23:37:06 +0300

    Added urls

--- apps/habits/urls.py
+++ apps/habits/urls.py
@@ -6,4 +6,9 @@ app_name = 'habits'
 
 urlpatterns = [
     path('', HabitListAPIView.as_view(), name='habits'),
+    path('public/', HabitPublicListAPIView.as_view(), name='habits_public'),
+    path('<int:pk>/', HabitDetailAPIView.as_view(), name='habit'),
+    path('create/', HabitCreateAPIView.as_view(), name='habits_create'),
+    path('update/<int:pk>/', HabitUpdateAPIView.as_view(), name='habit_update'),
+    path('delete/<int:pk>/', HabitDeleteAPIView.as_view(), name='habit_delete'),
 ]
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from apps.users.views import UserCreateAPIView
 
 app_name = 'users'
 
 urlpatterns = [
+    path('user/create/', UserCreateAPIView.as_view(), name='user_create'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
