a644b4bf4c91743bb203e99a9ca11f5cefb0e64b
Татьяна <tanechka201286grudeva@gmail.com>
2023-09-15 16:24:15 +0300

    add docker-compose.yaml

--- config/settings.py
+++ config/settings.py
@@ -88,9 +88,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Django_7',
+        'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'Zima-2023',
+        'PASSWORD': 'mysecretpassword',
+        'HOST': 'db',
     }
 }
 
